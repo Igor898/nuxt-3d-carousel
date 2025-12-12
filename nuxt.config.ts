e85981a69d14eaf01b4aa86ea47b1d22f5e02b30
Igor <potemkin_2002@bk.ru>
2025-12-12 19:48:40 +0300

    added gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  app: {
+    baseURL: '/nuxt-3d-carousel/'
+  },
   css: ['./assets/styles/_reset.sass']
 })
